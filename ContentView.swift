873c332c3c612e55d58b9f24d4d9317e103252b5
jojo <83029952+Anonymee55@users.noreply.github.com>
2024-06-20 21:08:53 +0200

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -1,1 +1,19 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Bonjour")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
 
